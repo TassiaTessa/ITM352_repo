50059181ff23b702edef8f9e9e5f00b109ad7723
Tassia <143228612+TassiaTessa@users.noreply.github.com>
2023-09-24 14:51:22 -1000

    adding lab 5 question one B webpage

---
+++ Lab6/SmartPhoneProducts1_4/Products_display2.js
@@ -0,0 +1,22 @@
+ 
+        
+    var store_name = "Tassia Corcoran";
+
+  
+
+ 
+    document.write(store_name + "'s Used Smart Phone Store");
+ 
+    function changeClassName(element){
+        
+    }
+
+ 
+    let hits = 0;
+    let spins = 1;
+    
+ 
+
+ 
+    document.write(store_name + "'s Used Smart Phone Store");
+ 
