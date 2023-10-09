a7d241a1810653bda027ae38ea7e060758d9f05d
Tassia <143228612+TassiaTessa@users.noreply.github.com>
2023-10-08 22:14:25 -1000

    uploading lab 7

--- Lab7/Products.js
+++ Lab7/Products.js
@@ -64,60 +64,74 @@ while(productCount <=num_products){
 }
 
 //exercise 2.2
-//NEED TO FIX THIS 
 console.log();
-console.log("exercise 2.2");
-productCount=1
-let lowerBound=num_products*.25;
-let upperBound=num_products*.25;
-
-while(productCount <= num_products){
-    if (productCount >= lowerBound && productCount<= upperBound){
-        console.log(eval('name'+productCount) + 'is sold out!');
+console.log("Exercise 2.2");
+productCount = 1;
+let lowerBound = num_products*.25;
+let upperBound = num_products*.75;
+while (productCount <= num_products){
+    if (productCount >= lowerBound && productCount <= upperBound){
+        console.log (eval('name' + productCount) + ' is sold out!!');
     } else {
-    console.log(productCount+'. '+eval('name'+productCount))
-        }
+    console.log(productCount +'. '+eval('name' + productCount));
+    }
     productCount++;
 }
 
 //exercise 2.3
-//NEED TO FIX THIS 
 console.log();
-console.log("exercise 2.3");
-productCount=1
-while(productCount <= num_products){
+console.log("Exercise 2.3");
+productCount = 1;
+while (productCount <= num_products){
     if (productCount >= num_products/2){
         console.log ("Don't ask for anything else");
-        process.exit();
+        //process.exit ();
     }
-    if (productCount >= lowerBound && productCount<= upperBound){
-        console.log(eval('name'+productCount) + 'is sold out!');
+    if (productCount >= lowerBound && productCount <= upperBound){
+        console.log (eval('name' + productCount) + ' is sold out!!');
     } else {
-    console.log(productCount+'. '+eval('name'+productCount))
-        }
+    console.log(productCount +'. '+eval('name' + productCount));
+    }
     productCount++;
 }
-//exercise 3.1
-console.log();
-console.log("exercise 3.1");
 
-productCount=1
-for(let i=1; i<=num_products; i++){
-    if (i >= lowerBound && i<= upperBound){
-        console.log(eval('name'+i) + 'is sold out!');
+/*exercise 3.1a - in the terminal it when ran it will say 
+1. HTC
+Apple is sold out!!
+Nokia is sold out!!
+4. Samsung
+5. Blackberry*/
+
+console.log();
+console.log("Exercise 3.1a");
+productCount = 1;
+for (let i=1; i<= num_products; i++) {
+    if (i >= lowerBound && i <= upperBound){
+        console.log (eval('name' + i) + ' is sold out!!');
     } else {
-    console.log(i+'. '+eval('name'+i))
-        }
+    console.log(i +'. '+eval('name' + i));
+    }
 }
 
 //exercise 3.1b
 console.log();
-console.log("exercise 3.1b");
-productCount=1
-for(let i=1; eval('typeof name'+i)!='undefined'; i++){
-    if (i >= lowerBound && i<= upperBound){
-        console.log(eval('name'+i) + 'is sold out!');
+console.log("Exercise 3.1b");
+for (let i=1; eval("typeof name" + i) != 'undefined'; i++) {
+    if (i >= lowerBound && i <= upperBound) {
+        console.log (eval('name' + i) + ' is sold out!!');
     } else {
-    console.log(i+'. '+eval('name'+i))
-        }
+    console.log(i +'. '+eval('name' + i));
+    }
 }
+
+//exercise 4.2
+//Create table header
+document.write('<table border="1"');
+document.write('<tr><th>Number</th><th>Item</th></tr>');
+//Loop to output rows
+for (let i=1; eval("typeof name" + i) != 'undefined'; i++) {
+    document.write(`<tr><td>${i}</td><td>${eval("name" +i)}</td></tr>`);
+}
+
+//Close the table
+document.write('</table>');
