0eb21db8264f2608e976ffc21826d1d8c2411207
Tassia <143228612+TassiaTessa@users.noreply.github.com>
2023-10-22 20:57:38 -1000

    Adding poke 10

---
+++ WODs/Poke10.1/Products_Data.js
@@ -0,0 +1,33 @@
+//define the quantitiy array 
+let quantity = [2,9,1.5,"cat",-1.5,0];
+
+//make an array of the products themselves
+let itemData = [
+    {
+        brand: 'Spicy ahi Poke',
+        price: 12.09,
+        quantityIndex: 0,
+    },
+    {
+        brand: 'Carne Asada Burrito',
+        price: 9.23,
+        quantityIndex: 1,
+    },
+    {
+        brand: 'Chicken Alfredo',
+        price: 22.89,
+        quantityIndex: 2,
+    },
+    {
+        brand: 'Papa Johns Pizza',
+        price: 0.01,
+        quantityIndex: 3,
+    },
+    {
+        brand: 'Potato Pierogi',
+        price: 35.00,
+        quantityIndex: 4,
+    },
+];
+
+export {itemData,quantity}
---
+++ WODs/Poke10.1/invoice3.js
@@ -0,0 +1,123 @@
+//Product Data
+import { itemData, quantity } from './Products_Data.js';
+
+//Variables for subtotal, tax, shipping charge, and total
+let subtotal = 0;
+let taxRate = 0.0575;
+let taxAmount = 0;
+let total = 0;
+let shippingCharge = 0;
+generateItemRows();
+
+if (subtotal <= 50) {
+  shippingCharge = 2;
+} else if (subtotal <= 100) {
+  shippingCharge = 5;
+} else {
+  shippingCharge = subtotal * 0.05;
+}
+
+//Calculate total with shipping
+taxAmount = subtotal * taxRate;
+total = subtotal + taxAmount + shippingCharge;
+
+//Setting total cell
+document.getElementById('total_cell').innerHTML = `$${total.toFixed(2)}`;
+
+//Setting subtotal, tax, and total cells
+document.getElementById('subtotal_cell').innerHTML = '$' + subtotal.toFixed(2);
+document.getElementById('tax_cell').innerHTML = '$' + taxAmount.toFixed(2);
+document.getElementByID('shipping_cell').innerHTML = '$' + shippingCharge.toFixed(2);
+
+//I tried a different method..but it didn't work
+function validateQuantity (quantity) {
+  if (isNaN(quantity)) {
+  return "Not a good food";//I know it should be not a number but changing this to make sal happy since he is from east coast lol
+  }else if (quantity < 0 && !Number.isInteger(quantity)) {
+  return "Negative Inventory and not an integer";
+  }else if (quantity <0){
+  return "Negative Inventory";
+  }else if (!Number.isInteger(quantity)){
+  return "Not an integer";
+  }else {
+  return "";
+  }
+  }
+  
+
+//function to generate table rows and apply quantity validation
+function generateItemRows() {
+  let table = document.getElementById('invoiceTable');
+  table.innerHTML = '';
+  let hasErrors = false;
+  
+  for (let i = 0; i < itemData.length; i++) {
+    let item = itemData[i];
+    let itemQuantity = quantity[item.quantityIndex];
+
+    let validationMessage = validateQuantity(itemQuantity);
+    if (validationMessage !== "") {
+      hasErrors = true;
+      let row = table.insertRow();
+      row.insertCell(0).innerHTML = item.brand;
+      row.insertCell(1).innerHTML = validationMessage;
+    } else if (itemQuantity > 0) {
+      let extendedPrice = item.price * itemQuantity;
+      subtotal += extendedPrice;
+
+      let row = table.insertRow();
+      row.insertCell(0).innerHTML = item.brand;
+      row.insertCell(1).innerHTML = itemQuantity;
+      row.insertCell(2).innerHTML = '$' + item.price.toFixed(2);
+      row.insertCell(3).innerHTML = '$' + extendedPrice.toFixed(2);
+    }
+    }
+    //if no error, display total
+    if (!hasErrors){
+      document.getElementById('total_cell').innerHTML = '$' + total.toFixed(2);
+    }
+  }
+
+    
+/*
+let row = table.insertRow();
+
+// Create a new row for each item
+row.insertCell(0).innerHTML = `${item1}`;
+row.insertCell(1).innerHTML = `${quantity1}`;
+row.insertCell(2).innerHTML = '$'+`${price1}`;
+row.insertCell(3).innerHTML = ('$' + `${extended_price1}`);
+row = table.insertRow(); 
+
+// Create a new row for each item
+row.insertCell(0).innerHTML = `${item2}`;
+row.insertCell(1).innerHTML = `${quantity2}`;
+row.insertCell(2).innerHTML = '$'+`${price2}`;
+row.insertCell(3).innerHTML = ('$' + `${extended_price2}`);
+row = table.insertRow(); 
+
+// Create a new row for each item
+row.insertCell(0).innerHTML = `${item3}`;
+row.insertCell(1).innerHTML = `${quantity3}`;
+row.insertCell(2).innerHTML = '$'+`${price3}`;
+row.insertCell(3).innerHTML = ('$' + `${extended_price3}`);
+row = table.insertRow(); 
+
+// Create a new row for each item
+row.insertCell(0).innerHTML = `${item4}`;
+row.insertCell(1).innerHTML = `${quantity4}`;
+row.insertCell(2).innerHTML = '$'+`${price4}`;
+row.insertCell(3).innerHTML = ('$' + `${extended_price4}`);
+row = table.insertRow();
+
+// Create a new row for each item
+row.insertCell(0).innerHTML = `${item5}`;
+row.insertCell(1).innerHTML = `${quantity5}`;
+row.insertCell(2).innerHTML = '$'+`${price5}`;
+row.insertCell(3).innerHTML = ('$' + `${extended_price5}`);
+
+// Set the subtotal, tax, and total cells
+document.getElementById('subtotal_cell').innerHTML = '$' + subtotal.toFixed(2);
+document.getElementById('tax_cell').innerHTML = '$' + taxAmount.toFixed(2);
+document.getElementById("shipping_cell").innerHTML = "$"+shipping.toFixed(2);
+document.getElementById('total_cell').innerHTML = '$' + total.toFixed(2);*/
